b4400447855e2e022ddb83432b6895f8fbc357f2
Matt Priour <mpriour@esri.com>
2021-06-24 17:05:18 -0500

    Remove debugger statements from components

--- src/components/webmap/WebMapView.tsx
+++ src/components/webmap/WebMapView.tsx
@@ -14,7 +14,6 @@ const WebMapView = ({
 }:IWebMapProps) => {
   const viewRef = useRef<HTMLDivElement>(null);
   const ViewContainer = <div ref={viewRef} {...props} />;
-  debugger;
   const map = new WebMap({
     portalItem: {
       id,
