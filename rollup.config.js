fe7b34f877b88876f8ea91ab7a5bdfe6f08deade
Matt Priour <mpriour@esri.com>
2021-06-24 11:38:53 -0500

    Remove arcgis core from the build, again

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default [
       }),
       postcss()
     ]
-  }/* ,
+  },
   {
     input: './src/index.ts',
     output: [
@@ -44,5 +44,5 @@ export default [
       }),
       postcss()
     ]
-  } */
+  }
 ]
